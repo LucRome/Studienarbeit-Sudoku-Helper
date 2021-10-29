c1db0a208e3cb09b9ab95fe76427d331c7878b19
LucRome <64947931+LucRome@users.noreply.github.com>
2021-10-29 16:42:09 +0200

    implement Sudoku Class - not tested yet

--- src/backend/sudoku/base.py
+++ src/backend/sudoku/base.py
@@ -4,6 +4,7 @@ Base Classes for Sudoku
 
 from typing import List, Optional
 from exceptions import *
+from math import floor
 
 class Field:
     """
@@ -39,4 +40,65 @@ class Field:
         return self.__candidates
     
 
+class Sudoku:
+    """
+    Class for the complete Sudoku Field
+    Orientation: See GitHub Wiki
+    """
+    fields: List[List[Field]] = list(list())
+    
+    def __init__(self) -> None:
+        for column in range(0, 8):
+            for row in range(0,8):
+                self.fields[column][row] = Field()
 
+    def get_field(self, row: int, column: int) -> Field:
+        if row < 0 or row > 8 or column < 0 or column > 8:
+            raise OutOfFieldsException()
+        return self.fields[column][row]
+
+    def get_row(self, row: int) -> List[Field]:
+        if row < 0 or row > 8:
+            raise OutOfFieldsException()
+        return self.fields[row]
+
+    def get_column(self, column: int) -> List[Field]:
+        if column < 0 or column > 8:
+            raise OutOfFieldsException()
+        ret: List[Field] = list()
+        for row in range(0, 8):
+            ret.append(self.fields[column][row])
+        return ret
+    
+    def get_block(self, block_nr: int) -> List[Field]:
+        if block_nr < 0 or block_nr > 8:
+            raise OutOfFieldsException()
+
+        # Determine the rows and columns
+        columns: range
+        if (block_nr % 3) == 0:
+            columns = range(0,2)
+        elif (block_nr % 3) == 1:
+            columns = range(3, 5)
+        else:
+            columns = range(6, 8)
+        rows: range
+        if block_nr <= 2:
+            rows = range(0,2)
+        elif block_nr <= 5:
+            rows = range(3, 5)
+        else:
+            rows = range(6, 8)
+        
+        # fill the return list
+        ret: List[Field] = list()
+        for x in columns:
+            for y in rows:
+                List.append(self.fields[columns][rows])
+
+    def get_block_nr(row: int, column: int) -> int:
+        if row < 0 or row > 8 or column < 0 or column > 8:
+            raise OutOfFieldsException()
+        x: int = floor(column/3)
+        y: int = floor(row/3)
+        return y*3 + x
--- src/backend/sudoku/exceptions.py
+++ src/backend/sudoku/exceptions.py
@@ -9,3 +9,7 @@ class WrongFieldValueExcetion(Exception):
 class WrongFieldCandidateException(Exception):
     def __init__(self, val: int):
         self.message = f"Error: Candidate Value {val} is too high or low.\nAllowed are numbers 0 - 9"
+
+class OutOfFieldsException(Exception):
+    def __init__(self):
+        self.message = f"Error: Out of the Sudoku Field.\nColumns, Rows and Blocks are Indexed with 0 - 8."
