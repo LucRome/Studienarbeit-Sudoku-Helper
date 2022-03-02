123cd05ca3e7e9d38af0a492e8849da1a4e4eb8a
LucRome <64947931+LucRome@users.noreply.github.com>
2022-03-02 17:01:50 +0100

    fix error with a backend check
    - would only check numbers 1 - 8 (and not 1 - 9)

--- src/backend/sudoku_helper/utils.py
+++ src/backend/sudoku_helper/utils.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from sudoku.base import NINE_RANGE
+from sudoku.base import NINE_RANGE, FIELD_VALUE_MAX, FIELD_VALUE_MIN
 from sudoku.base import Sudoku, Field
 from django.http import HttpRequest
 from typing import Tuple, Optional
@@ -38,7 +38,7 @@ def sudoku_simple_check(sudoku: Sudoku) -> Tuple[bool, Optional[str]]:
     # No double numbers
     for x in NINE_RANGE:
         [row, column, block] = [sudoku.get_row(x), sudoku.get_column(x), sudoku.get_block(x)]
-        for i in range(1,9):
+        for i in range(FIELD_VALUE_MIN, FIELD_VALUE_MAX + 1):
             if not ((get_amount_of_fields_with_value(i, row) <= 1) and (get_amount_of_fields_with_value(i, column) <= 1) and (get_amount_of_fields_with_value(i, block) <= 1)):
                 return (False, f'The Value {i} occurs multiple times in a row, a column or a block!')
     
