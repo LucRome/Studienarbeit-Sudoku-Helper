9a21c306f7567f2fd630db58ccac0109476110a6
noahhauke22 <71071583+noahhauke22@users.noreply.github.com>
2022-03-03 09:57:50 +0100

    test changes

--- src/backend/validation/validation.py
+++ src/backend/validation/validation.py
@@ -8,7 +8,7 @@ def field_to_value(field: Field) -> Optional[int]:
 def checkSudoku(grid):
     for row in range(0, 9):
         for col in range(0, 9):
-            if grid[row][col] == 0:
+            if (grid.get_field(row,col)).get_value() == None:
                 return False
 
     # We have a complete grid!
@@ -22,7 +22,7 @@ def validateSudoku(grid: Sudoku, counter):
   for i in range(0,81):
     row=i//9
     col=i%9
-    if (grid.get_field(row,col)).get_value == 0:
+    if (grid.get_field(row,col)).get_value() == None:
       for value in range (1, 10):
         #Check that this value has not already be used on this row
         if not (value in list(map(field_to_value,grid.get_row(row)))):
@@ -32,10 +32,10 @@ def validateSudoku(grid: Sudoku, counter):
             if not value in list(map(field_to_value,grid.get_block(Sudoku.get_block_nr(row=4, column=7)))):
               grid.get_field(row,col).set_value(value)
               if checkSudoku(grid):
-                grid.get_field(row,col).set_value(0)
+                grid.get_field(row,col).set_value(None)
                 return counter + 1
               else:
                 counter = validateSudoku(grid, counter)
-                grid.get_field(row,col).set_value(0)
+                grid.get_field(row,col).set_value(None)
 
       return counter
