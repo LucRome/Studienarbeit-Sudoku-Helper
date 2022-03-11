ff4b9a12543c4d211097250c6124805f23cbf32d
LucRome <64947931+LucRome@users.noreply.github.com>
2022-03-11 15:28:44 +0100

    remove debug outputs

--- src/backend/validation/validation.py
+++ src/backend/validation/validation.py
@@ -59,10 +59,8 @@ class Validation:
       fieldvalue = self.grid[row][col]
 
       if (fieldvalue == None):
-        print(f'checking field {row},{col}:')
         for value in (sudoku.get_field(row,col).get_candidates()):
           #Check that this value has not already be used on this row
-          print(f'\t value: {value}')
           if not (value in self.grid[row]):
             #Check that this value has not already be used on this column
             if not (value in (self.grid[0][col], self.grid[1][col], self.grid[2][col], self.grid[3][col], self.grid[4][col], self.grid[5][col], self.grid[6][col],
