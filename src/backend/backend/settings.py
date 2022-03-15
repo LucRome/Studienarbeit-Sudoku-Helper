3861778de81e29b63a9da8215ff064ef00daa5c0
LucRome <64947931+LucRome@users.noreply.github.com>
2022-03-15 21:10:10 +0100

    Remove dev_tools app

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sudoku_helper',
-    'dev_tools'
+    'sudoku_helper'
 ]
 
 MIDDLEWARE = [
--- src/backend/dev_tools/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DevToolsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dev_tools'
