0af22bcf810ad025fe810bba66a4441613e9619f
LucRome <64947931+LucRome@users.noreply.github.com>
2022-03-17 16:07:48 +0100

    Save
    - try to use JS to set size of candidates

--- src/backend/sudoku_helper/static/js/styling.js
+++ src/backend/sudoku_helper/static/js/styling.js
@@ -20,4 +20,17 @@ function resize_all() {
     fs_rest_str = size_rest + unit;
 
     $("p, span, button, a, strong").css("font-size", fs_rest_str)
+
+    // Resize Candidates
+    // First get size of the input fields
+    size_str = $($("#form-sudoku input")[1]).css("width");
+    unit = size_str.substr(size_str.length - 2);
+    size = size_str.substr(0, size_str.length - 2);
+
+    size_candidates = Number.parseFloat(size) / 3;
+    size_candidates_str = size_candidates + unit;
+    $(".candidate-table td").css("width", size_candidates_str);
+    $(".candidate-table td").css("height", size_candidates_str);
+
+    console.log(`inputs: ${size_str} \nCandidates: ${size_candidates_str}`)
 }
