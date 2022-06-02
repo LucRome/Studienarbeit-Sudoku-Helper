36ce54dcd55aa8221b5af121aba24ca25fd5a6d4
LucRome <64947931+LucRome@users.noreply.github.com>
2022-06-02 07:52:52 +0200

    add completion tests

--- src/backend/algorithms/test_completion.py
+++ src/backend/algorithms/test_completion.py
@@ -51,6 +51,21 @@ class TestCompleteSolvability(ut.TestCase):
     (Sudokus from: https://sudoku.com/de (mittel, experte, höllisch))
     """
 
+    def __test_sudokus(self, sudokus: List[Sudoku]):
+        """
+        Perform test operations for every sudoku in the List
+        """
+        for sudoku in sudokus:
+            sudoku.select_candidates()
+
+            # first validation
+            self.assertTrue(verify(sudoku))
+
+            # attempt solving
+            success, msg = try_to_solve(sudoku)
+            self.assertTrue(success, msg=msg)
+
+
     def test_simple(self):
         """
         Test a simple sudoku
@@ -76,4 +91,76 @@ class TestCompleteSolvability(ut.TestCase):
         self.assertTrue(success, msg=msg)
 
 
-    # TODO: Problem with some algorithm
+    def test_middle(self):
+        """
+        Test mediocre sudokus (Expert Level on Website)
+        """
+        sudokus = [
+            Sudoku([
+                [None, None, 4, None, None, 1, None, None, 8],
+                [None, None, None, None, None, None, None, None, 7],
+                [3, None, None, 4, None, None, None, None, None],
+                [1, None, None, 2, None, 6, None, None, 9],
+                [None, None, None, None, 3, 8, 7, None, None],
+                [None, 2, None, None, None, None, None, 1, None],
+                [None, 8, None, 3, None, None, None, 2, None],
+                [None, 6, None, None, 1, None, None, None, None],
+                [None, 7, None, None, None, None, None, 6, 5],
+            ]),
+            Sudoku([
+                [None, None, None, None, 1, None, None, None, None],
+                [2, None, None, None, 3, None, None, 9, None],
+                [3, None, 1, None, None, 4, None, None, None],
+                [None, 8, None, 7, None, 5, None, None, None],
+                [None, None, 4, None, None, None, None, 5, None],
+                [None, 9, None, None, None, None, None, None, 2],
+                [None, None, None, 6, None, None, 4, None, None],
+                [None, None, None, None, None, None, 3, 2, 1],
+                [None, None, None, 4, 8, None, 9, None, None],
+            ])
+        ]
+
+        self.__test_sudokus(sudokus)
+
+
+    def test_hard(self):
+        """
+        Test hard sudokus (hell level on Website)
+        """
+        sudokus = [
+            Sudoku([
+                [None, None, 1, None, None, None, None, None, 7],
+                [None, 9, None, None, None, 6, 1, 3, None],
+                [None, None, None, 3, None, None, None, None, 4],
+                [None, 6, None, None, 2, None, None, None, None],
+                [None, None, None, None, None, None, None, None, 1],
+                [None, None, 9, 7, None, None, 5, 8, None],
+                [None, None, 5, 8, None, None, 3, 9, None],
+                [8, None, None, None, None, 7, None, None, None],
+                [None, None, None, None, None, None, None, 4, None],
+            ]),
+            Sudoku([
+                [None, None, None, 3, None, None, None, None, None],
+                [8, None, None, None, None, None, None, None, 6],
+                [None, None, 5, None, 1, 9, 4, None, None],
+                [None, None, None, None, None, 2, None, None, None],
+                [None, None, 1, None, 7, 5, 9, None, None],
+                [None, 9, None, None, None, None, None, 4, None],
+                [None, None, None, 2, None, None, 7, None, None],
+                [None, None, 3, 4, None, None, None, None, None],
+                [5, None, None, None, 3, 7, None, 8, None],
+            ]),
+            Sudoku([
+                [None, None, 6, None, 3, None, None, 5, 2],
+                [None, 1, None, None, None, 7, None, None, None],
+                [None, None, None, None, None, None, 4, None, None],
+                [None, None, 2, None, 5, None, None, 8, 3],
+                [None, None, None, None, None, None, None, None, 9],
+                [6, None, None, 2, None, None, None, None, None],
+                [None, None, 8, None, None, None, 9, None, None],
+                [None, None, None, None, 4, None, 6, None, None],
+                [7, None, None, None, None, 3, None, 4, 8],
+            ])
+        ]
+
+        self.__test_sudokus(sudokus)
